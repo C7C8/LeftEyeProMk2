e060c80e222f26d01e3a728b09dcf83a819ce32d
c7c8 <me@crmyers.dev>
2024-04-02 14:15:19 -0400

    Implement carousel component from crmyers.dev

--- src/app/common/components/carousel/carousel.component.ts
+++ src/app/common/components/carousel/carousel.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, Input } from '@angular/core';
+import {
+	NguCarousel,
+	NguCarouselConfig,
+	NguCarouselDefDirective,
+	NguCarouselItemDirective, NguCarouselNextDirective, NguCarouselPointDirective, NguCarouselPrevDirective,
+	NguItemComponent
+} from "@ngu/carousel";
+import { NgForOf, NgIf } from "@angular/common";
+import _ from "lodash";
+import { MatIconButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+
+export interface CarouselImage {
+	image: string;
+	alt: string;
+	caption?: string;
+}
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
+	imports: [
+		NguCarousel,
+		NguItemComponent,
+		NguCarouselItemDirective,
+		NguCarouselDefDirective,
+		NgIf,
+		NguCarouselPrevDirective,
+		NguCarouselNextDirective,
+		NguCarouselPointDirective,
+		NgForOf,
+		MatIconButton,
+		MatIcon
+	],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss'
 })
-export class CarouselComponent {
+export class CarouselComponent implements AfterViewInit {
+	@Input() images: CarouselImage[] = [];
+	@Input() showButtons: boolean = true;
+	@Input() showPoints: boolean = true;
+	@Input() shuffle: boolean = false;
+	@Input() carouselConfig: NguCarouselConfig = {
+		grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+		slide: 2,
+		speed: 600,
+		animation: 'lazy',
+		interval: { initialDelay: 10000, timing: 10000 },
+		loop: true,
+		point: {
+			visible: true
+		},
+		load: 2,
+		touch: false,
+		easing: 'ease',
+	};
 
+	constructor(private cdr: ChangeDetectorRef) {
+		if (this.shuffle) {
+			this.images = _.shuffle(this.images)
+		}
+	}
+
+	ngAfterViewInit(): void {
+		this.cdr.detectChanges();
+	}
 }

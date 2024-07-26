771fe528815c1f7f5a2a9fa38d63842822193fa8
c7c8 <me@crmyers.dev>
2024-07-26 15:39:03 -0400

    Add konami directive

---
+++ src/app/common/directives/konami.directive.ts
@@ -0,0 +1,44 @@
+import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
+
+/**
+ * Detects when the Konami code has been entered.
+ *
+ * Adapted from https://dev.to/rfornal/angular-konami-code-3fk
+ */
+@Directive({
+  selector: '[konami]',
+  standalone: true
+})
+export class KonamiDirective {
+
+	@Output() private konami: EventEmitter<void> = new EventEmitter<void>();
+	private sequence: string[] = [];
+	private konamiCode: string[] = [
+		'arrowup', 'arrowup',
+		'arrowdown', 'arrowdown',
+		'arrowleft', 'arrowright',
+		'arrowleft', 'arrowright',
+		'b', 'a'
+	];
+
+	constructor() {}
+
+	@HostListener('window:keydown', ['$event'])
+	handleKeyboardEvent(event: KeyboardEvent) {
+		if (event.key) {
+			this.sequence.push(event.key.toLowerCase());
+
+      if (this.sequence.length > this.konamiCode.length) {
+				this.sequence.shift();
+			}
+
+      if (this.isKonamiCode()) {
+				this.konami.emit();
+			}
+		}
+	}
+
+	isKonamiCode(): boolean {
+		return this.konamiCode.every((code: string, index: number) => code === this.sequence[index]);
+	}
+}

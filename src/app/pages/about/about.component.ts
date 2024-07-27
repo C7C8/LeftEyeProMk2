5f5e641f063c39a9a720563b62d87035bcf8eddc
c7c8 <me@crmyers.dev>
2024-07-27 16:53:55 -0400

    Add about page w/photos

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader } from "@angular/material/card";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+	imports: [
+		MatCard,
+		MatCardHeader,
+		MatCardContent
+	],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })

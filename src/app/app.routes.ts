447ba407adb1512b2d9122d8da9810577d187aac
c7c8 <me@crmyers.dev>
2024-07-26 21:27:20 -0400

    Fix routes always directing to homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { GalleryComponent } from "./pages/gallery/gallery.component";
+import { OtherProductsComponent } from "./pages/other-products/other-products.component";
+import { LatestAdventuresComponent } from "./pages/latest-adventures/latest-adventures.component";
+import { CurrentProjectComponent } from "./pages/current-project/current-project.component";
+import { ContactComponent } from "./pages/contact/contact.component";
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent, title: "Left Eye Productions by Steven Kazlowski" },
-	{ path: 'gallery', component: HomeComponent, title: "Left Eye Productions - Gallery",},
-	{ path: 'other-products', component: HomeComponent, title: "Left Eye Productions - Other Products" },
-	{ path: 'about', component: HomeComponent, title: "Left Eye Productions - About the Photographer" },
-	{ path: 'latest-adventures', component: HomeComponent, title: "Left Eye Productions - Latest Adventures" },
-	{ path: 'current-project', component: HomeComponent, title: "Left Eye Productions - Current Project" },
-	{ path: 'contact', component: HomeComponent, title: "Left Eye Productions - Contact" },
+	{ path: 'about', component: AboutComponent, title: "Left Eye Productions - About the Photographer" },
+	{ path: 'gallery', component: GalleryComponent, title: "Left Eye Productions - Gallery",},
+	{ path: 'other-products', component: OtherProductsComponent, title: "Left Eye Productions - Other Products" },
+	{ path: 'latest-adventures', component: LatestAdventuresComponent, title: "Left Eye Productions - Latest Adventures" },
+	{ path: 'current-project', component: CurrentProjectComponent, title: "Left Eye Productions - Current Project" },
+	{ path: 'contact', component: ContactComponent, title: "Left Eye Productions - Contact" },
 ];

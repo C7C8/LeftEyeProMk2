1e28b81b7827c88eff106801774930acd6b66011
c7c8 <me@crmyers.dev>
2024-07-31 14:48:53 -0400

    First pass at more comprehensive data model for image indexes

--- src/app/common/services/search/defs.ts
+++ src/app/common/services/search/defs.ts
@@ -1,13 +1,44 @@
-export interface ImageData {
+/*
+ * This file specifies data structures used for browser-side indexing & searching of images.
+ *
+ * SIDECAR files are files that have a one-to-one mapping with another file or concept. Their paths are not known at
+ * runtime and there are arbitrarily many of them.
+ *
+ * INDEX files are single-occurrence files with known paths, mapping specific concepts to list of sidecar files. There
+ * are fewer index files than sidecar files.
+ *
+ *
+ * SIDECAR files have the naming convention "[name].sidecar.yaml"
+ * INDEX files have the naming convention "[name].index.yaml"
+ *
+ */
+
+/************
+ * SIDECARS *
+ ************/
+
+export interface ImageSidecar {
 	/**
-	 * URL to a low-res preview of the image.
+	 * URL to a low-res preview of the image, used in large grids of images where high resolution is not a priority.
 	 */
 	thumbnail: string;
+
+	/**
+	 * URL to a high-res but low detail preview of the image in SVG format. This should be blurred to hide that it's
+	 * actually a low-poly SVG in disguise. Ideal for showing in large-format previews of images while the tiled version
+	 * loads in.
+	 */
+	svg?: string;
 
 	/**
 	 * List of tags applicable to the image.
 	 */
 	tags: string[];
+
+	/**
+	 * Medium to long form description of the image.
+	 */
+	description?: string;
 
 	/**
 	 * The year the image was created in.
@@ -25,19 +56,44 @@ export interface ImageData {
 	yTiles?: number;
 }
 
-export interface Category {
+export interface TagSidecar {
 	/**
-	 * Human-readable name for the category.
+	 * Human-readable name for the tag.
 	 */
 	name: string;
 
 	/**
-	 * Human-readable description for the category.
+	 * Human-readable description for the tag.
 	 */
-	description: string;
+	description?: string;
 
 	/**
-	 * List of images within this category.
+	 * List of images within this tag.
 	 */
-	images?: ImageData[];
+	image_sidecars: string[];
 }
+
+export interface CategorySidecar {
+	name: string;
+	description: string;
+	associatedTags: string[];
+}
+
+/***********
+ * INDEXES *
+ ***********/
+
+export interface TagIndex {
+	/**
+	 * Map of tags to their tag sidecars
+	 */
+	[name: string]: string;
+}
+
+export interface CategoriesIndex {
+	/**
+	 * Map of categories to lists of tags
+	 */
+	[name: string]: string[];
+}
+
--- src/app/common/services/search/image.service.ts
+++ src/app/common/services/search/image.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Category } from "./defs";
 
 @Injectable({
 	providedIn: "root"
@@ -9,13 +8,4 @@ export class ImageService {
 	// private cachedCategories: Category[];
 
 	constructor(private http: HttpClient) {}
-
-	public getCategories(): Category[] {
-		// TODO: Replace with code for fetching categories from JSON (or from cache if already loaded)
-		return [
-			{ name: "Polar Bears", description: "Photos of polar bears" },
-			{ name: "Marine life", description: "Photos of marine life" },
-			{ name: "Forest life", description: "Elk, deer, brown & black bears, and more" }
-		];
-	}
 }
